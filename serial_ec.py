b66cb13b7862f5a0cf18c66d2ba4c8b2629e0205
Mansuang Pawong <mansaung@hotmail.com>
2017-01-23 15:06:58 +0700

    fix serial_ec

--- serial_ec.py
+++ serial_ec.py
@@ -1,7 +1,7 @@
 import serial
-# serialport = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
-# serialport.write("status\r\n")
-#serialport.write("gpio1 0")
-# response = serialport.readlines()
-response = ['\r\n', '3.97,23.56,5.50,0,0,0,1,1\r\n'];
+serialport = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
+serialport.write("status\r\n")
+#erialport.write("gpio1 0")
+response = serialport.readlines()
+#response = ['\r\n', '3.97,23.56,5.50,0,0,0,1,1\r\n'];
 print response
